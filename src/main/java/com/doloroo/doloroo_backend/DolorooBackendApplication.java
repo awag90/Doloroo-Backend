0a98574386ef610435dbaaf62c1be0ea00a3781e
Andreas Wagner <andreasw.90@protonmail.com>
2021-11-20 17:00:21 +0100

    Setup Project

---
+++ src/main/java/com/doloroo/doloroo_backend/DolorooBackendApplication.java
@@ -0,0 +1,13 @@
+package com.doloroo.doloroo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolorooBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolorooBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doloroo/doloroo_backend/DolorooBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doloroo.doloroo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolorooBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
