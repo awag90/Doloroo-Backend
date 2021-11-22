4e4394bbf67a193539831723b73492ba863c58a7
Andreas Wagner <andreasw.90@protonmail.com>
2021-11-22 11:10:44 +0100

    Added Dose Constructor Getter and Setter

--- src/main/java/com/doloroo/doloroo_backend/medication/Dose.java
+++ src/main/java/com/doloroo/doloroo_backend/medication/Dose.java
@@ -3,4 +3,25 @@ package com.doloroo.doloroo_backend.medication;
 public class Dose {
     private float dose;
     private Unit unit;
+
+    public Dose(float dose, Unit unit) {
+        this.dose = dose;
+        this.unit = unit;
+    }
+
+    public float getDose() {
+        return dose;
+    }
+
+    public void setDose(float dose) {
+        this.dose = dose;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Unit unit) {
+        this.unit = unit;
+    }
 }
